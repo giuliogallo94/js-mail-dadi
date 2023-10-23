8bc2109bc76363a3d7faa222c5dd5e9891c5af3c
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-24 00:49:01 +0200

    add: html print

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,12 +2,23 @@ const userDice = Math.floor(Math.random() * 12) + 1;
 const pcDice = Math.floor(Math.random() * 12) + 1;
 console.log(userDice, pcDice);
 
+const emojiWinElem = document.getElementById("win");
+const emojiLoseElem = document.getElementById("lose");
+const emojiTieElem = document.getElementById("tie");
+
 let result = "";
 if (userDice > pcDice) {
-  result = `L'utente vince con un punteggio di ${userDice} contro ${pcDice} del PC`;
+  result = `L'Utente vince con un punteggio di ${userDice} contro ${pcDice}`;
+  emojiWinElem.classList.remove(`display_none`);
 } else if (userDice === pcDice) {
   result = `Utente e PC pareggiano con un punteggio di ${userDice}`;
+  emojiTieElem.classList.remove("display_none");
 } else if (userDice < pcDice) {
-  result = `PC vince con un punteggio di ${pcDice} contro ${userDice} dell'utente`;
+  result = `PC vince con un punteggio di ${pcDice} contro ${userDice}`;
+  emojiLoseElem.classList.remove("display_none");
 }
 console.log(result);
+
+document.getElementById("user_dice").innerHTML = `${userDice}`;
+document.getElementById("pc_dice").innerHTML = `${pcDice}`;
+document.getElementById("result").innerHTML = `${result}`;
