68eab7142bc08d85a004e86cfd6afdb43648fb12
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-23 15:59:41 +0200

    add: mail exercise

---
+++ mail/README.md
@@ -0,0 +1,11 @@
+# ESERCIZIO
+
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+**Svolgimento**
+
+1. Tramite un prompt chiedere all'user la propria mail
+2. Attraverso la condizione IF contorllare che la mail sia nella lista di chi può accedere
+3. Al termine del controllo stampare un messaggio di benvenuto se la mail risulta avente diritto di accesso, o un messaggio di errore in caso contrario
---
+++ mail/js/script.js
@@ -0,0 +1,33 @@
+const mailsList = [
+  "kylianmbappe@gmail.com",
+  "cristianoronaldo@gmail.com",
+  "leomessi@gmail.com",
+  "francescototti@gmail.com",
+  "rafaleao@gmail.com",
+  "robertlewa@gmail.com",
+  "erlinghaaland@gmail.com",
+  "nicobarella@gmail.com",
+  "dusanvlao@gmail.com",
+  "oliviergiroud@gmail.com",
+  "judebellingham@gmail.com",
+];
+
+let mailUser = prompt("Fornire la propria mail");
+console.log(mailUser);
+
+let found = false;
+
+for (let i = 0; i < mailsList.length; i++) {
+  const curMail = mailsList[i];
+  if (curMail === mailUser) {
+    found = true;
+  }
+
+  console.log(curMail, mailUser, found);
+}
+
+if (found === true) {
+  alert(`Bentornato ${mailUser}`);
+} else {
+  alert("Inserire una mail valida");
+}
